1adb069de7f32c586ad9e1a91b13c07ad7be2267
Maciej Gołaszewski <jodator@jodator.net>
2023-12-12 16:05:44 +0100

    🦌 Fix GH pages deployment URL

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/secret-private-santa/',
+  base: '/private-secret-santa/',
 })
