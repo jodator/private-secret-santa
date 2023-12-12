873ec4b78ce7ebc0abe6c51b413e25d7aecf9a85
Maciej Gołaszewski <jodator@jodator.net>
2023-12-12 17:35:03 +0100

    🔥 Remove Puzzle SDK

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
 import './index.css'
 
-import { configureConnection } from '@puzzlehq/sdk'
 import { HashRouter } from 'react-router-dom'
-
-configureConnection({
-  dAppName: 'Aleo Playground',
-  dAppDescription: 'Create and manage your own custom token.',
-  dAppUrl: 'http://localhost:5173/',
-  dAppIconURL: 'https://link.to/assets/your_logo.png',
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
